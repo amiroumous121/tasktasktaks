1c6b1f2c418ead39598506e1def18fcfd5c38222
amer121 <109520654+amiroumous121@users.noreply.github.com>
2023-09-26 22:36:04 +1000

    Add files via upload

---
+++ src/components/RecipesList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button @click="addRecipe">Add Recipe</button>
+
+    <div class="recipes">
+      <RecipeItem v-for="recipe in recipes" :key="recipe.id" :recipe="recipe" />
+
+      <button v-for="recipe in recipes" @click="deleteRecipe(recipe.id)">
+        Delete {{ recipe.title }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import RecipeItem from "./RecipeItem.vue";
+
+export default {
+  props: ["recipes", "addRecipe", "deleteRecipe"],
+
+  methods: {
+    addRecipe() {
+      this.$emit("addRecipe");
+    },
+
+    deleteRecipe(id) {
+      this.$emit("deleteRecipe", id);
+    },
+  },
+};
+</script>
+
+<style>
+/* styles */
+</style>
